1e236d6a6afc327cab714d02ad5afb615f590a8d
Alex Nunn <alex.nunn@pm.me>
2024-01-03 15:30:41 -0800

    Add documentation strings for external interace

--- src/kernels.jl
+++ src/kernels.jl
@@ -8,7 +8,7 @@ using SpecialFunctions
 using Serialization
 using Interpolations
 
-export K
+export K, ∂xK, ∂yK, ∂zK
 
 # Components for computing convolution kernel
 I = (ρ, z) -> begin 
@@ -52,27 +52,68 @@ end
 
 
 # Convolution kernels for evaluating the potential and associated derivatives
-function K(ρ, z)
-    return (I_fast(ρ, z) - I_fast(ρ, 2 - z)) / 4π^2
-end
+"""
+    K(x, y, z)
 
+Planar electrostatic trap kernel in cartesian coordinates where `0 < z < 1` and
+`x`, `y` are real valued horizontal coordinates. The bottom and top plates of
+the trap are located at `z = 0` and `z = 1`, respectively.
+
+Useful for computing the electrostatic field via convolution.
+
+See also [`∂xK`](@ref), [`∂yK`](@ref), [`∂zK`](@ref).
+"""
 function K(x, y, z)
     ρ = sqrt(x^2 + y^2)
     return (I_fast(ρ, z) - I_fast(ρ, 2 - z)) / 4π^2
 end
 
+"""
+    K(ρ, z)
+
+Planar electrostatic trap kernel in cylindrical coordinates where `0 < ρ`,
+`0 < z < 1`.
+"""
+function K(ρ, z)
+    return (I_fast(ρ, z) - I_fast(ρ, 2 - z)) / 4π^2
+end
+
+"""
+    ∂xK(x, y, z)
+
+Partial derivative wrt. `x` of planar electrostatic trap kernel in cartersian 
+coordinates where `0 < z < 1`.
+
+See also [`K`](@ref)
+"""
 ∂xK = (x, y, z) -> begin
     ρ = sqrt(x^2 + y^2)
     if ρ < 1e-12 return 0.0 end
     return (x / ρ) * (∂ρI_fast(ρ, z) - ∂ρI_fast(ρ, 2 - z)) / 4π^2
 end
 
+"""
+    ∂yK(x, y, z)
+
+Partial derivative wrt. `y` of planar electrostatic trap kernel in cartersian 
+coordinates where `0 < z < 1`.
+
+See also [`K`](@ref)
+"""
 ∂yK = (x, y, z) -> begin
     ρ = sqrt(x^2 + y^2)
     if ρ < 1e-12 return 0.0 end
     return (y / ρ) * (∂ρI_fast(ρ, z) - ∂ρI_fast(ρ, 2 - z)) / 4π^2
 end
 
+"""
+    ∂zK(x, y, z)
+
+Partial derivative wrt. `z` of planar electrostatic trap kernel in cartersian 
+coordinates where `0 < z < 1`.
+
+See also [`K`](@ref)
+"""
 ∂zK = (x, y, z) -> begin
     ρ = sqrt(x^2 + y^2)
     return (∂zI_fast(ρ, z) + ∂zI_fast(ρ, 2 - z)) / 4π^2
