6e3f5b084ed2cf2a933f31ac2f1c7995b7d2fc3b
Documenter.jl <documenter@juliadocs.github.io>
2024-01-03 02:10:21 +0000

    build based on 02de369

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = uESTplanar","category":"page"},{"location":"#uESTplanar","page":"Home","title":"uESTplanar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for uESTplanar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [uESTplanar]","category":"page"}]
+}
